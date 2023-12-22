b81816c4fead5ff35cb18d948e4459001d254521
jocarsa <info@josevicentecarratala.com>
2023-12-22 10:43:58 +0100

    miniaturas y menu

--- functions.php
+++ functions.php
@@ -2,7 +2,9 @@
 
     function register_menus() {
         register_nav_menu('menuprincipal', __('Primary Menu'));
+        register_nav_menu('redessociales', 'Navegación secundaria');
     }
+
     add_action('after_setup_theme', 'register_menus');
     add_theme_support('post-thumbnails');
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <h3><?php the_title(); ?></h3>
 <div class="contenido">
-    <?php the_content(); ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article>
+            <?php
+            if (has_post_thumbnail()) {
+                // Display the featured image
+                the_post_thumbnail();
+            }
+            ?>
+            <h3>
+                <?php the_title(); ?>
+            </h3>
+                
+            <div class="contenido">
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; endif; ?>
 </div>
-<?php endwhile; endif; ?>
-<?php get_footer(); ?>        
-        
+<?php get_footer(); ?>
