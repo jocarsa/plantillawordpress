b3217465e012a5dd3e7933e9dc11363347c2a627
jocarsa <info@josevicentecarratala.com>
2023-12-22 10:06:27 +0100

    viewport

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 <!doctype html>
 <html>
     <head>
-          <meta charset="<?php bloginfo('charset'); ?>">  
+        <meta charset="<?php bloginfo('charset'); ?>">  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
     
