b45345e358e05ba53fa041a01be273d10f2b9ac1
jocarsa <info@josevicentecarratala.com>
2023-12-22 10:08:56 +0100

    clase del body

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
         <meta charset="<?php bloginfo('charset'); ?>">  
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php wp_title(); ?></title>
-        <?php get_header(); ?>
+        
     </head>
-    <body>
+    <body <?php body_class(); ?>>
     
     </body>
 </html>
