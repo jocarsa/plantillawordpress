6abddaa7076c9545cadfbd1c698161b027461b95
jocarsa <info@josevicentecarratala.com>
2023-12-22 11:37:31 +0100

    filtrar posts

--- index.php
+++ index.php
@@ -31,36 +31,43 @@
             <div class="contenido">
                 <?php the_content(); ?>
                 <?php
-                if (is_front_page()) {
-                    $args = array(
-                        'post_type' => 'post', // Specify the post type you want to display (e.g., 'post' for standard posts).
-                        'posts_per_page' => -1, // Display all posts on the front page.
-                    );
-                    $front_page_query = new WP_Query($args);
+                $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => -1,
+                    'category_name' => 'portafolio', // Specify the category slug you want to display
+                );
+                $portafolio_query = new WP_Query($args);
 
-                    if ($front_page_query->have_posts()) {
-                        echo 'Categories: ';
-
-                        while ($front_page_query->have_posts()) {
-                            $front_page_query->the_post();
-                            $categories = get_the_category();
-
-                            if ($categories) {
-                                foreach ($categories as $category) {
-                                    echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a>';
-
-                                    // Add a comma and space for multiple categories, except for the last one
-                                    if ($category !== end($categories)) {
-                                        echo ', ';
-                                    }
-                                }
+                if ($portafolio_query->have_posts()) {
+                    while ($portafolio_query->have_posts()) {
+                        $portafolio_query->the_post();
+                        ?>
+                        <article class="entrada">
+                            <?php
+                            if (has_post_thumbnail()) {
+                                // Display the featured image with a link to the post
+                                echo '<a href="' . esc_url(get_permalink()) . '">';
+                                the_post_thumbnail();
+                                echo '</a>';
                             }
-                        }
+                            ?>
+                            <h3>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h3>
 
-                        wp_reset_postdata(); // Reset the post data query
+                            <div class="contenido">
+                                <?php the_content(); ?>
+                            </div>
+                        </article>
+                        <?php
                     }
+
+                    wp_reset_postdata(); // Reset the post data query
                 }
-                ?>
+            
+            ?>
             </div>
         </article>
     <?php endwhile; endif; ?>
