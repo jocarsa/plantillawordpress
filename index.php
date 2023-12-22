637a83b65a36e56c11fa33df0941084a41c8c74c
jocarsa <info@josevicentecarratala.com>
2023-12-22 10:16:50 +0100

    hoja de estilo

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">  
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php wp_title(); ?></title>
-        
+        <title><?php bloginfo('name'); ?></title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     </head>
     <body <?php body_class(); ?>>
         <header>
