f50c8ad41696bbc4830c618dbd23ba0511ae0fe7
jocarsa <info@josevicentecarratala.com>
2023-12-22 10:55:13 +0100

    detectar frontal

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 <div class="contenido">
+    <?php
+        if (is_front_page()) {
+    ?>
+        <div id="banner">
+            <h3>Desarrrollo personalizado de aplicaciones</h3>
+            <h4>Convertimos tus proyectos en realidad</h4>
+        </div>
+    <?php
+        } 
+    ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article class="<?php if (is_page()) {
                             echo "pagina";
