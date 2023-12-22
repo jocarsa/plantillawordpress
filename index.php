466c84a79abac1dfed2443f1959d0d4112919cb8
jocarsa <info@josevicentecarratala.com>
2023-12-22 10:07:03 +0100

    titulo

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">  
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php wp_title(); ?></title>
     </head>
     <body>
     
