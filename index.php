16be7e38acbcc5a9b6c11da19b12873fa8c9133a
jocarsa <info@josevicentecarratala.com>
2023-12-22 11:32:12 +0100

    categorias

--- index.php
+++ index.php
@@ -30,6 +30,37 @@
                 
             <div class="contenido">
                 <?php the_content(); ?>
+                <?php
+                if (is_front_page()) {
+                    $args = array(
+                        'post_type' => 'post', // Specify the post type you want to display (e.g., 'post' for standard posts).
+                        'posts_per_page' => -1, // Display all posts on the front page.
+                    );
+                    $front_page_query = new WP_Query($args);
+
+                    if ($front_page_query->have_posts()) {
+                        echo 'Categories: ';
+
+                        while ($front_page_query->have_posts()) {
+                            $front_page_query->the_post();
+                            $categories = get_the_category();
+
+                            if ($categories) {
+                                foreach ($categories as $category) {
+                                    echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a>';
+
+                                    // Add a comma and space for multiple categories, except for the last one
+                                    if ($category !== end($categories)) {
+                                        echo ', ';
+                                    }
+                                }
+                            }
+                        }
+
+                        wp_reset_postdata(); // Reset the post data query
+                    }
+                }
+                ?>
             </div>
         </article>
     <?php endwhile; endif; ?>
