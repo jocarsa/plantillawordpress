09ea4ed51f19791dd99052a2bcbf0c4e2651dd6a
jocarsa <info@josevicentecarratala.com>
2023-12-22 10:12:42 +0100

    estructura

--- index.php
+++ index.php
@@ -12,11 +12,20 @@
         
     </head>
     <body <?php body_class(); ?>>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h3><?php the_title(); ?></h3>
-        <div class="contenido">
-            <?php the_content(); ?>
-        </div>
-        <?php endwhile; endif; ?>
+        <header>
+            
+        </header>
+        <main>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <h3><?php the_title(); ?></h3>
+            <div class="contenido">
+                <?php the_content(); ?>
+            </div>
+            <?php endwhile; endif; ?>
+        
+        </main>    
+        <footer>
+            <?php wp_footer(); ?>
+        </footer>
     </body>
 </html>
